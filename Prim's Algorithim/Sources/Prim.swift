133e9293f77cac76fbc5fd1d25496e97bc719406
Arian Flores <arian@arianflores.com>
2022-07-17 16:44:25 -0700

    Filled - Prim's Algorithm

--- Prim's Algorithim/Sources/Prim.swift
+++ Prim's Algorithim/Sources/Prim.swift
@@ -1,1 +1,48 @@
+import Foundation
+enum Prim<Graph: DataStructures.Graph> where Graph.Element: Hashable {
+    typealias Edge = Graph.Edge
+    typealias Vertex = Edge.Vertex
+    typealias MinimumSpanTree = AdjacencyList<Graph.Element>
+    
+    static func getMinimumSpanningTree(for graph: Graph) -> (cost: Double, minimumSpanTree: MinimumSpanTree) {
+        var cost = 0.0
+        var minimumSpanTree = MinimumSpanTree(verticies: graph.vertices)
+        
+        guard let start = graph.vertices.first else {
+            return (cost, minimumSpanTree)
+        }
+        
+        var visited = Set<Vertex>()
+        var priorityQueue = PriorityQueue<Edge> { $0.weight < $1.weight }
+        
+        func enqueueAllAvaliableEdges(for source: Vertex) {
+            graph.getEdges(from: source)
+                .filter {
+                    !visited.contains($0.destination)
+                }
+                .forEach {
+                    priorityQueue.enqueue($0)
+                }
+        }
+        
+        visited.insert(start)
+        enqueueAllAvaliableEdges(for: start)
+        
+        while let lightestEdge = priorityQueue.dequeue() {
+            let destination = lightestEdge.destination
+            
+            guard !visited.contains(destination) else {
+                continue
+            }
+            
+            visited.insert(destination)
+            cost += lightestEdge.weight
+            minimumSpanTree.add(lightestEdge)
+            
+            enqueueAllAvaliableEdges(for: destination)
+        }
 
+        return (cost, minimumSpanTree)
+    }
+    
+}
