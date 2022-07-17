a5b88721cc4e43eb0aa8a26cdbc1dfa696d123a2
Arian Flores <arian@arianflores.com>
2022-07-17 13:41:16 -0700

    Filled - Stacks and Queues

--- Stacks and Queues/Sources/Stack.swift
+++ Stacks and Queues/Sources/Stack.swift
@@ -1,1 +1,44 @@
+struct Stack<Element: Equatable>: Equatable {
+    static func == (lhs: Stack<Element>, rhs: Stack<Element>) -> Bool {
+        lhs.storage == rhs.storage
+    }
+    
+    private var storage = [Element]()
+    
+    var isEmpty: Bool  {
+        return peek() == nil
+    }
+    
+    init() {}
+    
+    init(_ elements: [Element]) {
+        storage = elements
+    }
+    
+    func peek() -> Element? {
+        return storage.last
+    }
+    
+    mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    @discardableResult
+    mutating func pop() -> Element? {
+        return storage.popLast()
+    }
+}
 
+extension Stack: CustomDebugStringConvertible {
+    var debugDescription: String {
+        return storage
+            .map { "\($0)"}
+            .joined(separator: " ")
+    }
+}
+
+extension Stack: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        storage = elements
+    }
+}
--- Stacks and Queues/Tests/StackTestCase.swift
+++ Stacks and Queues/Tests/StackTestCase.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import DataStructures
 
 final class StackTestCase: XCTestCase {
-  var stack = Stack<Int>()
+    var stack = Stack<Int>()
   
   override func setUp() {
     stack.push(1)
@@ -12,7 +12,7 @@ final class StackTestCase: XCTestCase {
   }
   
   func test_push() {
-    XCTAssertEqual(stack.description, "1 2 3 4")
+    XCTAssertEqual(stack.debugDescription, "1 2 3 4")
   }
   
   func test_pop() {
@@ -24,7 +24,7 @@ final class StackTestCase: XCTestCase {
   }
   
   func test_isEmpty() {
-    XCTAssertFalse(stack.isEmpty())
+    XCTAssertFalse(stack.isEmpty)
   }
   
   func test_initWithArray() {
@@ -36,4 +36,51 @@ final class StackTestCase: XCTestCase {
     let stack: Stack = ["blueberry", "plain", "potato"]
     XCTAssertEqual(stack, ["blueberry", "plain", "potato"])
   }
+  
+  func test_reverse_the_stack() {
+      func printInReverse<T:Equatable>(_ array: [T]) -> String {
+          guard !array.isEmpty else {
+              return ""
+          }
+          var stack = Stack<T>()
+          var result = ""
+
+          for value in array {
+            stack.push(value)
+          }
+
+          while let value = stack.pop() {
+              result.append(" \(value)")
+          }
+          
+          return result
+      }
+      XCTAssertEqual(printInReverse([1,2,3,4]), " 4 3 2 1")
+      
+      let anotherArray = ["blueberry", "plain", "potato"]
+      XCTAssertEqual(printInReverse(anotherArray), " potato plain blueberry")
+  }
+    
+  func test_is_balanced_parens() {
+      func isBalancedParens(_ input: String) -> Bool {
+          var stack = Stack<Character>()
+
+          for character in input {
+            if character == "(" {
+              stack.push(character)
+            } else if character == ")" {
+              if stack.isEmpty {
+                return false
+              } else {
+                stack.pop()
+              }
+            }
+          }
+          
+          return stack.isEmpty
+        }
+        
+        XCTAssertTrue(isBalancedParens("h((e))llo(world)()"))
+        XCTAssertFalse(isBalancedParens("(hello world"))
+    }
 }
