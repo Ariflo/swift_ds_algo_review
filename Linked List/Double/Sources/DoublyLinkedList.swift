e72f4d513bf2a41274e7e3ed33d1e972af9a1628
Arian Flores <arian@arianflores.com>
2022-08-19 13:53:10 -0700

    Filled - Linked List

--- Linked List/Double/Sources/DoublyLinkedList.swift
+++ Linked List/Double/Sources/DoublyLinkedList.swift
@@ -1,1 +1,83 @@
+class DoublyLinkedList<Value> {
+    var head: Node<Value>?
+    var tail: Node<Value>?
+    
+    init() {}
+    
+    var isEmpty: Bool {
+        head == nil
+    }
+    
+    var first: Node<Value>? {
+        head
+    }
+    
+    func append(_ value: Value) {
+        let newNode = Node(value: value)
+        
+        guard let tailNode = tail else {
+            head = newNode
+            tail = newNode
+            return
+        }
+        
+        newNode.previous = tailNode
+        tailNode.next = newNode
+        tail = newNode
+    }
+    
+    func remove(_ node: Node<Value>) -> Value {
+        let prev = node.previous
+        let next = node.next
+        
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        
+        next?.previous = prev
+        
+        if next == nil {
+            tail = prev
+        }
+        
+        node.previous = nil
+        node.next = nil
+        
+        return node.value
+    }
+}
 
+extension DoublyLinkedList: CustomStringConvertible {
+    var description: String {
+        var string = ""
+        var current = head
+        while let node = current {
+            string.append("\(node.value) -> ")
+            current = node.next
+        }
+        return string + "end"
+    }
+}
+
+class LinkedListIterator<Value>: IteratorProtocol {
+    var current: Node<Value>?
+    
+    init(node: Node<Value>?) {
+        current = node
+    }
+    
+    func next() -> Node<Value>? {
+        defer {
+            current = current?.next
+        }
+        return current
+    }
+}
+
+extension DoublyLinkedList: Sequence {
+    func makeIterator() -> LinkedListIterator<Value> {
+        LinkedListIterator(node: head)
+    }
+}
--- Linked List/Double/Sources/Node.swift
+++ Linked List/Double/Sources/Node.swift
@@ -1,1 +1,19 @@
+class Node<Value> {
+    var value: Value
+    var next: Node<Value>?
+    var previous: Node<Value>?
+    
+    init(value: Value){
+        self.value = value
+    }
+}
 
+extension Node:CustomStringConvertible {
+    var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        
+        return "\(value) -> " + String(describing: next) + " "
+    }
+}
